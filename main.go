610a18c055123a18bb2a97181b19c38b740336bb
llowkeysam <omransamuel@gmail.com>
2024-06-12 17:56:41 +0300

    Graceful Shutdown

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/llowkeysam/go/application"
 )
@@ -10,7 +12,10 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		fmt.Printf("failed to start app:", err)
 	}
